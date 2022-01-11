34e2f99c7d931178d224f8b656a6fad5c27a6f5f
SaidRavest <sravest@externos.bancoestado.cl>
2022-01-11 18:10:44 -0300

    manejo de errores

--- DESAFIO-2/index.js
+++ DESAFIO-2/index.js
@@ -4,25 +4,42 @@ class Contenedor {
         producto.id =  Date.now()
         const data = this.getAll()
         data.push(producto)
-        fs.writeFileSync('productos.txt', JSON.stringify(data,null,4))
-        return producto.id
+        try {
+            fs.writeFileSync('productos.txt', JSON.stringify(data,null,4))
+            return producto.id
+        } catch (error) {
+            throw new Error('No se pudo guardar el producto')
+        }
+        
     }
     getById(id) {
         const data = this.getAll()
         return data.find(producto => producto.id === id) || null
     }
     getAll() {
-        const data = fs.readFileSync('productos.txt', 'utf-8')
-        return JSON.parse(data)
+        try {
+            const data = fs.readFileSync('productos.txt', 'utf-8')
+            return JSON.parse(data)
+        } catch (error) {
+            throw new Error('No se pudo leer el archivo')
+        }   
     }
     deleteById(id) {
         const data = this.getAll()
         const deleted = data.filter(producto => producto.id !== id)
-        fs.writeFileSync('productos.txt', JSON.stringify(deleted))
+        try{
+            fs.writeFileSync('productos.txt', JSON.stringify(deleted))
+        }catch(error){
+            throw new Error('No se pudo eliminar el producto')
+        }
     }
     deleteAll(){
         const data = []
+        try{
         fs.writeFileSync('productos.txt', JSON.stringify(data))    
+        }catch(error){
+            throw new Error('No se pudo eliminar el producto')
+        }
     }
 }
 
