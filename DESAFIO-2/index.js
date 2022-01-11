5d9145a03c299c2b3d18e077b4641dfbf6f0372e
SaidRavest <sravest@externos.bancoestado.cl>
2022-01-11 18:27:52 -0300

    se separo en dos archivos

--- DESAFIO-2/index.js
+++ DESAFIO-2/index.js
@@ -1,52 +1,17 @@
-const fs = require('fs')
-class Contenedor {
-    save(producto) {
-        producto.id =  Date.now()
-        const data = this.getAll()
-        data.push(producto)
-        try {
-            fs.writeFileSync('productos.txt', JSON.stringify(data,null,4))
-            return producto.id
-        } catch (error) {
-            throw new Error('No se pudo guardar el producto')
-        }
-        
-    }
-    getById(id) {
-        const data = this.getAll()
-        return data.find(producto => producto.id === id) || null
-    }
-    getAll() {
-        try {
-            const data = fs.readFileSync('productos.txt', 'utf-8')
-            return JSON.parse(data)
-        } catch (error) {
-            throw new Error('No se pudo leer el archivo')
-        }   
-    }
-    deleteById(id) {
-        const data = this.getAll()
-        const deleted = data.filter(producto => producto.id !== id)
-        try{
-            fs.writeFileSync('productos.txt', JSON.stringify(deleted))
-        }catch(error){
-            throw new Error('No se pudo eliminar el producto')
-        }
-    }
-    deleteAll(){
-        const data = []
-        try{
-        fs.writeFileSync('productos.txt', JSON.stringify(data))    
-        }catch(error){
-            throw new Error('No se pudo eliminar el producto')
-        }
-    }
-}
+const  Contenedor =  require('./Contenedor')
+const p = new Contenedor([])
 
-const p = new Contenedor()
-
-p.save({'title':"said"})
-console.log(p.getById(1641934276649))
+//guardar producto
+p.save({
+    'title':"said",
+    "price":100,
+    "thumbnail":"https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png"
+})
+//buscar elemento por id
+console.log(p.getById(1641936295804))
+//eliminar elemento por id
 p.deleteById(1641933911054)
-p.deleteAll()
+//traer todo
 console.log(p.getAll())
+//eliminar todos los elementos
+p.deleteAll()
