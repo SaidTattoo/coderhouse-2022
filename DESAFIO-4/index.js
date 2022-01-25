1f79df54b495303ea26293560edada478a7301fc
SaidTattoo <said.tattoo@gmail.com>
2022-01-25 13:56:40 -0300

    correccion de rutas

--- DESAFIO-4/index.js
+++ DESAFIO-4/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/api/', productosRouter);
+app.use('/api/productos', productosRouter);
 app.use('/static', express.static('public'))
 
 app.listen(8080,() => {
--- DESAFIO-4/routes/index.js
+++ DESAFIO-4/routes/index.js
@@ -4,33 +4,33 @@ const Contenedor = require('../service/Contenedor');
 const productosRouter = Router()
 const p = new Contenedor()
 
-productosRouter.get('/productos', (req, res) => {
+productosRouter.get('/', (req, res) => {
     let resp = p.getAll().length !== 0
         ? { productos: p.getAll() }
         : { error: 'producto no encontrado' }
     res.json(resp)
 })
-productosRouter.get('/productos/:id', (req, res) => {
+productosRouter.get('/:id', (req, res) => {
     const { id } = req.params;
     let resp = p.getById(parseInt(id))
     ? { productos:p.getById(parseInt(id)) }
     : { error: 'producto no encontrado' }
     res.json( resp )
 })
-productosRouter.post('/productos', (req, res) => {
+productosRouter.post('/', (req, res) => {
     let resp = p.save(req.body)
     ? { productos: p.save(req.body)}
     : { error: 'error al guardar el producto' }
     res.json( resp )
 })
-productosRouter.put('/productos/:id', (req, res) => {
+productosRouter.put('/:id', (req, res) => {
     const { id } = req.params;
     let resp = p.update(parseInt(id), req.body)
     ? { productos: p.update(parseInt(id), req.body) }
     : { error: 'error al actualizar el producto' }
     res.json( resp )
 })
-productosRouter.delete('/productos/:id', (req, res) => {
+productosRouter.delete('/:id', (req, res) => {
     const { id } = req.params;
     res.json({
         msg: p.deleteById(parseInt(id))
