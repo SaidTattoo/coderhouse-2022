9aabff45fa874f25ac26a7651a0af7468c239c16
SaidTattoo <said.tattoo@gmail.com>
2022-01-25 13:51:23 -0300

    desafio 4

--- DESAFIO-4/index.js
+++ DESAFIO-4/index.js
@@ -4,7 +4,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+
 app.use('/api/', productosRouter);
 app.use('/static', express.static('public'))
-app.listen(8080)
+
+app.listen(8080,() => {
+    console.log('Servidor corriendo en el puerto 8080')
+}).on( 'error',(err) => console.log(err))
+
+
 
