e850dd74377e470fdc11289c731ebd8410193783
SaidRavest <sravest@externos.bancoestado.cl>
2022-02-01 03:13:35 -0300

    pug

--- DESAFIO-5/index.js
+++ DESAFIO-5/index.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const productosRouter = require('./routes');
-const  { engine } = require('express-handlebars');
 const Contenedor = require('./service/Contenedor');
 const app = express();
 const c = new Contenedor()
-app.engine("hbs", engine({
-    extname: "hbs",
-    defaultLayout: "index.hbs",
-    layoutsDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + "/views/partials/"
-}))
+require('pug');
+//configuracion de pug
 app.set("views", "./views")
-app.set("view engine", "hbs")
+app.set("view engine", 'pug')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/productos', productosRouter);
 
 app.get("/",(req, res) => {
-    res.render("crearProducto")
+    res.render("crearProducto.pug")
 })
 
 app.get('/productos/vista',(req, res) => {
     const data = c.getAll()
-    res.render("main", data.length !== 0 ? { productos:data } : { error: 'no hay productos cargados' })
+    res.render("main.pug", data.length !== 0 ? { productos:data } : { error: 'no hay productos cargados' })
 })
 
 app.listen(8080, () => {
